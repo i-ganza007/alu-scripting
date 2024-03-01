322b61e2e4fa40dea6882caee6a001ab2c9bf224
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-03-01 17:03:38 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+import requests
+
+
+def top_ten(subreddit):
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
+        print(None)
