c79e365654f00f2221889e23f740a7358042ad25
i-ganza007 <i.ganza@alustudent.com>
2024-03-01 05:44:52 -0800

    api_advanced

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Return the number of subscribers of a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that fetches number_of_subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+    return 0
